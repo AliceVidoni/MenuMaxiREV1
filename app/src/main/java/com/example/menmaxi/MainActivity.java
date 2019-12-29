cffb7597556562c1d67524b582d1447e77dd8ba9
AliceVidoni <52039281+AliceVidoni@users.noreply.github.com>
2019-12-29 14:21:13 +0100

    Aggiunta pagine
    Aggiunta pagina home (inserimento tavolo)

--- app/src/main/java/com/example/menmaxi/MainActivity.java
+++ app/src/main/java/com/example/menmaxi/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.menmaxi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.num_tavolo);
+    }
+
+    public void nuovaPagina(View v) {
+        Intent paginaCategorie = new Intent(this, SelectCategoria.class);
+        //paginaCategorie.putExtra("Numero_Tavolo", num_tavolo);
+        startActivity(paginaCategorie);
     }
 }
---
+++ app/src/main/java/com/example/menmaxi/SelectCategoria.java
@@ -0,0 +1,14 @@
+package com.example.menmaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_categoria);
+    }
+}
