7b947104f1f886f9f3c5aa3dcf01098b8cd799ed
AliceVidoni <52039281+AliceVidoni@users.noreply.github.com>
2020-01-01 15:26:45 +0100

    Aggiunta interfacce
    Interfaccia per la scelta del metodo di pagamento e interfaccia per comunicare al clienta che la richiesta di conto è andata a buon fine

---
+++ app/src/main/java/com/example/menmaxi/MetodoPagamento.java
@@ -0,0 +1,21 @@
+package com.example.menmaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MetodoPagamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodo_pagamento);
+    }
+
+    public void confermaRichiesta(View v) {
+        Intent richiesta= new Intent(this, RichiestaConfermata.class);
+        startActivity(richiesta);
+    }
+}
