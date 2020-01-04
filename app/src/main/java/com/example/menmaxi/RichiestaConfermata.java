76a97ee2da56e5f9e054add530a0706fca7f74f0
AliceVidoni <52039281+AliceVidoni@users.noreply.github.com>
2020-01-04 15:57:11 +0100

    Aggiunta
    Aggiunta icona (anche se non so perchè non si vede), e modifica classe

--- app/src/main/java/com/example/menmaxi/RichiestaConfermata.java
+++ app/src/main/java/com/example/menmaxi/RichiestaConfermata.java
@@ -2,13 +2,24 @@ package com.example.menmaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class RichiestaConfermata extends AppCompatActivity {
+public class RichiestaConfermata extends AppCompatActivity implements Runnable{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_richiesta_confermata);
+        Handler mHandler =new Handler();
+        mHandler.postDelayed(this, 8000);
+    }
+
+
+    @Override
+    public void run() {
+        Intent intent=new Intent (this, MainActivity.class);
+        startActivityForResult(intent, 100);
     }
 }
